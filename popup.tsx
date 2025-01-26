005f656e51efa2660c5123c8ce907cf94120a480
21subham <subhamrajbhandari@gmail.com>
2025-01-26 15:37:45 +0545

    indic

--- popup.tsx
+++ popup.tsx
@@ -6,14 +6,28 @@ function IndexPopup() {
   const [data, setData] = useState("")
 
   return (
-    <div
-      style={{
-        padding: 16
-      }}>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <p>{data}</p>
-      <p>{getNepaliFromEnglish(data)}</p>
-    </div>
+    <>
+      {" "}
+      <div
+        style={{
+          padding: 16
+        }}>
+        <p>"Nepali-react-transliterate":</p>
+        <input onChange={(e) => setData(e.target.value)} value={data} />
+        <p>{getNepaliFromEnglish(data)}</p>
+      </div>
+      <div>
+        <iframe src="https://www.ashesh.com.np/linknepali-unicode3.php?api=642116p323"></iframe>
+        <span>
+          <a
+            href="https://www.ashesh.com.np/nepali-unicode.php"
+            title="Nepali unicode"
+            target="_top">
+            Nepali Unicode
+          </a>
+        </span>
+      </div>
+    </>
   )
 }
 
